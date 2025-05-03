505244c42fcda18733a2195d8b2eca66acbb81da
Doug Vieira <dougmvieira@gmail.com>
2025-05-04 00:49:59 +0100

    Add treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -19,6 +19,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     spec = {
         'killitar/obscure.nvim',
+        { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
     },
     -- disable `luarocks` support completely
     rocks = { enabled = false },
@@ -33,3 +34,20 @@ vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })
+
+-- Setup treesitter
+require('nvim-treesitter.configs').setup {
+    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+    ensure_installed = { "lua", "python" },
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+    -- Automatically install missing parsers when entering buffer
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+}
